07b6bb17b6bb7546dc81deb0e7661402b32ff0aa
Humbal Shahi <sh.humble@gmail.com>
2017-09-06 09:46:09 -0700

    added update angularjs file and created file directory

--- themes/custom/cityplanning/js/api/negativeDeclarationCtrl.js
+++ themes/custom/cityplanning/js/api/negativeDeclarationCtrl.js
@@ -1,10 +1,3 @@
-/**
- * File: negativeDeclarationCtrl.js
- * author: Humbal Shahi
- * Email: humbal.shahi@lacity.org
- * Created date: 05/25/2017
- */
-
 (function () {
     "use strict";
 
@@ -12,259 +5,38 @@
     	.config(function($interpolateProvider) {
     		$interpolateProvider.startSymbol('{[{').endSymbol('}]}');
 		})
-       /*.service('NegativeDeclaration', function ($http) {
-    	   this.getData = function() {
-    		   return $http.get('./negative-declaration.json');
-    	   }
-       })*/
-       .controller('negativeDeclarationCtrl', function ($scope, $timeout) {
+       
+       .controller('negativeDeclarationCtrl', function ($scope, $timeout, $http, filterFilter) {
         	$scope.enable = "false";
         	$scope.loadingText = "Loading ...";
-        	
+        	$scope.pageSize = 3;
+			$scope.currentPage = 1;
+			// $scope.x = [];
+
             var self = this;
-            self.data = [
-                     	{
-                    		"date": "2017-02-23",
-                    		"negativeDeclaration": "NG-17-025-PL",
-                    		"caseNumber": "ENV-2016-1040",
-                    		"address": "1005-1013 Everett St; Silver Lake-Echo Park-Elysian Valleye",
-                    		"counsilDistrict": "CD1",
-                    		"caseNumbers": [
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_022317.htm#1040",
-                    				"caseNumber": "ENV-2016-1040",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_022317/ENV-2016-1040.pdf"
-                    			},
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_022317.htm#1040",
-                    				"caseNumber": "ENV-2016-1040-A",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_022317/ENV-2016-1040-A.pdf"
-                    			},
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_022317.htm#1040",
-                    				"caseNumber": "ENV-2016-1040-B",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_022317/ENV-2016-1040-B.pdf"
-                    			},
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_022317.htm#1040",
-                    				"caseNumber": "ENV-2016-1040-C-1",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_022317/ENV-2016-1040-C-1.pdf"
-                    			},
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_022317.htm#1040",
-                    				"caseNumber": "ENV-2016-1040-C-2",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_022317/ENV-2016-1040-C-2.pdf"
-                    			},
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_022317.htm#1040",
-                    				"caseNumber": "ENV-2016-1040-D-1",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_022317/ENV-2016-1040-D-1.pdf"
-                    			},
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_022317.htm#1040",
-                    				"caseNumber": "ENV-2016-1040-D-2",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_022317/ENV-2016-1040-D-2.pdf"
-                    			},
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_022317.htm#1040",
-                    				"caseNumber": "ENV-2016-1040-D-3",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_022317/ENV-2016-1040-D-3.pdf"
-                    			},
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_022317.htm#1040",
-                    				"caseNumber": "ENV-2016-1040-D-4",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_022317/ENV-2016-1040-D-4.pdf"
-                    			},
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_022317.htm#1040",
-                    				"caseNumber": "ENV-2016-1040-E-1",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_022317/ENV-2016-1040-E-1.pdf"
-                    			},
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_022317.htm#1040",
-                    				"caseNumber": "ENV-2016-1040-E-2",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_022317/ENV-2016-1040-E-2.pdf"
-                    			}
-                    		]
-                    	},
-                    	{
-                    		"date": "2017-02-23",
-                    		"negativeDeclaration": "NG-17-026-PL",
-                    		"caseNumber": "ENV-2016-2477",
-                    		"address": "1370, 1374, 1410, & 1416-1418 S Flower St; Central City",
-                    		"counsilDistrict": "CD14",
-                    		"caseNumbers": [
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_022317.htm#1040",
-                    				"caseNumber": "ENV-2016-2477",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_022317/ENV-2016-2477.pdf"
-                    			},
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_022317.htm#1040",
-                    				"caseNumber": "ENV-2016-2477-Appendices",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_022317/ENV-2016-2477-Appendices.pdf"
-                    			}
-                    		]
-                    	},
-                    	{
-                    		"date": "2017-02-23",
-                    		"negativeDeclaration": "NG-16-365-PL",
-                    		"caseNumber": "ENV-2016-2995",
-                    		"address": "3242 W 8th St & 811 S Mariposa Ave; Wilshire",
-                    		"counsilDistrict": "CD10",
-                    		"caseNumbers": [
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_022317.htm#1040",
-                    				"caseNumber": "ENV-2016-2995",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_022317/ENV-2016-2995.pdf"
-                    			}
-                    		]
-                    	},
-                    	{
-                    		"date": "2017-02-16",
-                    		"negativeDeclaration": "NG-17-021-PL",
-                    		"caseNumber": "ENV-2016-1081",
-                    		"address": "929 E 2nd St; Central City North",
-                    		"counsilDistrict": "CD14",
-                    		"caseNumbers": [
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_021617.htm#1081",
-                    				"caseNumber": "ENV-2016-1081",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_021617/ENV-2016-1081.pdf"
-                    			},
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_021617.htm#1081",
-                    				"caseNumber": "ENV-2016-1081-A",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_021617/ENV-2016-1081-A.pdf"
-                    			},
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_021617.htm#1081",
-                    				"caseNumber": "ENV-2016-1081-B",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_021617/ENV-2016-1081-B.pdf"
-                    			},
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_021617.htm#1081",
-                    				"caseNumber": "ENV-2016-1081-C",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_021617/ENV-2016-1081-C.pdf"
-                    			},
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_021617.htm#1081",
-                    				"caseNumber": "ENV-2016-1081-D",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_021617/ENV-2016-1081-D.pdf"
-                    			},
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_021617.htm#1081",
-                    				"caseNumber": "ENV-2016-1081-E",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_021617/ENV-2016-1081-E.pdf"
-                    			},
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_021617.htm#1081",
-                    				"caseNumber": "ENV-2016-1081-F",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_021617/ENV-2016-1081-F.pdf"
-                    			},
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_021617.htm#1081",
-                    				"caseNumber": "ENV-2016-1081-G",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_021617/ENV-2016-1081-G.pdf"
-                    			}
-                    		]
-                    	},
-                    	{
-                    		"date": "2017-02-16",
-                    		"negativeDeclaration": "NG-17-020-PL",
-                    		"caseNumber": "ENV-2016-273",
-                    		"address": "3401-3437 N Pasadena Ave & 123-167 W Ave. 34; Northeast Los Angeles",
-                    		"counsilDistrict": "CD1",
-                    		"caseNumbers": [
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_021617.htm#273",
-                    				"caseNumber": "ENV-2016-273",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_021617/ENV-2016-273.pdf"
-                    			},
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_021617.htm#273",
-                    				"caseNumber": "ENV-2016-273-A-B",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_021617/ENV-2016-273-A-B.pdf"
-                    			},
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_021617.htm#273",
-                    				"caseNumber": "ENV-2016-273-C-D",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_021617/ENV-2016-273-C-D.pdf"
-                    			},
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_021617.htm#273",
-                    				"caseNumber": "ENV-2016-273-E-G",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_021617/ENV-2016-273-E-G.pdf"
-                    			},
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_021617.htm#273",
-                    				"caseNumber": "ENV-2016-273-H-J",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_021617/ENV-2016-273-H-J.pdf"
-                    			}
-                    		]
-                    	},
-                    	{
-                    		"date": "2017-02-16",
-                    		"negativeDeclaration": "NG-17-022-PL",
-                    		"caseNumber": "ENV-2016-4078",
-                    		"address": "2110 W Sunset Blvd; Silver Lake-Echo Park-Elysian Valley",
-                    		"counsilDistrict": "CD13",
-                    		"caseNumbers": [
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_021617.htm#273",
-                    				"caseNumber": "ENV-2016-4078",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_021617/ENV-2016-4078.pdf"
-                    			}
-                    		]
-                    	},
-                    	{
-                    		"date": "2017-02-16",
-                    		"negativeDeclaration": "NG-17-023-PL",
-                    		"caseNumber": "ENV-2016-4118",
-                    		"address": "5701 N Etiwanda Ave; Encino-Tarzana",
-                    		"counsilDistrict": "CD3",
-                    		"caseNumbers": [
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_021617.htm#4078",
-                    				"caseNumber": "ENV-2016-4118",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_021617/ENV-2016-4118.pdf"
-                    			}
-                    		]
-                    	},
-                    	{
-                    		"date": "2017-02-16",
-                    		"negativeDeclaration": "NG-17-024-PL",
-                    		"caseNumber": "ENV-2016-4553",
-                    		"address": "4939 E York Blvd; Northeast Los Angeles",
-                    		"counsilDistrict": "CD14",
-                    		"caseNumbers": [
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_021617.htm#4553",
-                    				"caseNumber": "ENV-2016-4553",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_021617/ENV-2016-4118.pdf"
-                    			}
-                    		]
-                    	},
-                    	{
-                    		"date": "2017-02-09",
-                    		"negativeDeclaration": "NG-17-018-PL",
-                    		"caseNumber": "ENV-2014-4943",
-                    		"address": "992 S Arapahoe St; Wilshire",
-                    		"counsilDistrict": "CD1",
-                    		"caseNumbers": [
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_020917.htm#4943",
-                    				"caseNumber": "ENV-2014-4943",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_020917/ENV-2014-4943.pdf"
-                    			},
-                    			{
-                    				"laTimesURL": "https://planning.lacity.org/eir/publication/Pub_020917.htm#4943",
-                    				"caseNumber": "ENV-2014-4943-A",
-                    				"doc": "http://cityplanning.lacity.org/staffrpt/mnd/Pub_020917/ENV-2014-4943-A.pdf"
-                    			}
-                    		]
-                    	}
-                    ];
+               // Extract data from json file
+               $http({
+                   method: 'POST',
+                   url: siteurl + '/file/notice.json',
+               }).then( function(response) {
+                   $scope.events = response;
+                   self.data = response.data;
+                   $scope.total = response.data.length;
+                   $scope.data =  response.data;
+               });
+
+
+           // $watch search to update pagination
+           $scope.noOfPages = Math.ceil($scope.total / $scope.pageSize);
+           $scope.$watch('negativeDeclaration', function (newVal, oldVal) {
+               $scope.filtered = filterFilter($scope.data, newVal);
+               $scope.total = $scope.filtered.length;
+               $scope.noOfPages = Math.ceil($scope.total / $scope.pageSize);
+               $scope.currentPage = 1;
+           }, true);
+               
+
+            // self.data = [{"caseNumber":"NG-17-030-PL","address":"<p>1370, 1374, 1410, &amp; 1416-1418 S Flower St; Kathmandu City<\/p>\r\n","date":"2017-09-13","counsilDistrict":"CD21","caseNumbers":[{"laTimesURL":"http:\/\/example.com","caseNumber":"Test Case NO","doc":"http:\/\/localhost\/drupal\/scratch\/sites\/default\/files\/2017-09\/pdf-1.pdf"}]},{"caseNumber":"NG-17-026-PL","address":"<p>1370, 1374, 1410, &amp; 1416-1418 S Flower St; Central City<\/p>\r\n","date":"2017-02-23","counsilDistrict":"CD14","caseNumbers":[{"laTimesURL":"http:\/\/www.cnn.com\/2017\/08\/28\/politics\/north-korea-launch-unidentified-projectile\/index.html","caseNumber":"ENV-2016-2477","doc":"http:\/\/localhost\/drupal\/scratch\/sites\/default\/files\/2017-08\/pdf-1_2.pdf"}]},{"caseNumber":"NG-17-025-PL","address":"<p>1005-1013 Everett St; Silver Lake-Echo Park-Elysian Valleye<\/p>\r\n","date":"2017-02-23","counsilDistrict":"CD1","caseNumbers":[{"laTimesURL":"http:\/\/humble.com.np\/beta2\/archieves\/pdf-1.pdf","caseNumber":"ENV-2016-1040","doc":"http:\/\/localhost\/drupal\/scratch\/sites\/default\/files\/2017-08\/pdf-1.pdf"},{"laTimesURL":"http:\/\/www.cnn.com\/2017\/08\/28\/politics\/north-korea-launch-unidentified-projectile\/index.html","caseNumber":"ENV-2016-1040-A","doc":"http:\/\/localhost\/drupal\/scratch\/sites\/default\/files\/2017-08\/pdf-1_0.pdf"},{"laTimesURL":"http:\/\/www.cnn.com\/2017\/08\/28\/politics\/north-korea-launch-unidentified-projectile\/index.html","caseNumber":"ENV-2016-1040-B","doc":"http:\/\/localhost\/drupal\/scratch\/sites\/default\/files\/2017-08\/pdf-1_1.pdf"}]}];
 
             self.expandAll = function (expanded) {
                 // $scope is required here, hence the injection above, even though we're using "controller as" syntax
@@ -275,6 +47,8 @@
         		$scope.enable = "true";
                 $scope.loadingText = "";
             }, 2000);
+            
+            console.log($scope.data);
 
         })
         .directive('expand', function () {
@@ -286,5 +60,11 @@
                     });
                 }]
             };
-        });
+        })
+        .filter('startFrom', function(){
+			return function(data, start){
+				start = 0 + start;
+				return data.slice(start);
+			}
+		});
 }());
