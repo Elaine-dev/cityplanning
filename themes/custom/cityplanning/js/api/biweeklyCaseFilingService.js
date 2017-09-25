257f5dfe7a026557bcfc14684986a588c907f31b
Humbal Shahi <sh.humble@gmail.com>
2017-09-25 15:04:43 -0700

    added and deleted old file

--- themes/custom/cityplanning/js/api/biweeklyCaseFilingService.js
+++
@@ -1,5 +0,0 @@
-app.service('biweeklyCaseFilingService', function($http) {
-	this.getList = function(year) {
-		return $http.get("http://10.68.8.109:2016/ladcp/api/v1/bi-weekly-case-filing/"+year);
-	}
-});
