f3fbbbce849d16950cff071d44e6e83407828eb9
Humbal Shahi <sh.humble@gmail.com>
2017-09-25 15:14:34 -0700

    updated ctrl and services

--- themes/custom/cityplanning/js/api/biweeklyCaseReportCtrl.js
+++ themes/custom/cityplanning/js/api/biweeklyCaseReportCtrl.js
@@ -1,6 +1,6 @@
 app.controller('biweeklyCaseReportCtrl', function($scope, biweeklyCaseReportService, $filter, $timeout, $http, $location) {
 	var id = $location.absUrl().split('/')[6];		// get cpc from url in the form of query parameter.
-	id = (id) ? id : 154;
+	id = (id) ? id : 163;
 
 	$scope.years = ['2017', '2016', '2015', '2014', '2013', '2012'];
 	$scope.date = $filter('date')(new Date(), 'yyyy');		// get current year
@@ -9,12 +9,12 @@ app.controller('biweeklyCaseReportCtrl', function($scope, biweeklyCaseReportServ
 	// display Bi-Weekly Case Filing by Certified Neighborhood Council
 	biweeklyCaseReportService.getBiweeklyCaseFiling(id).then (function (response) {
 		$scope.biweeklyCaseFiling = response.data;
-		if (id == 154) {
-			$scope.pageTitle = 'Bi-Weekly Case Filing by Certified Neighborhood Council';	//154
-		} else if (id == 155) {
-			$scope.pageTitle = 'Bi-Weekly Case Filing Activity by City Council District';	// 155
+		if (id == 163) {
+			$scope.pageTitle = 'Bi-Weekly Case Filing by Certified Neighborhood Council';	//163
+		} else if (id == 173) {
+			$scope.pageTitle = 'Bi-Weekly Case Filing Activity by City Council District';	//173
 		} else {
-			$scope.pageTitle = 'Bi-Weekly Case Filing by Community Plan Area';				// 156
+			$scope.pageTitle = 'Bi-Weekly Case Filing by Community Plan Area';				//183
 		}
 	});
 });
--- themes/custom/cityplanning/js/api/biweeklyCaseReportService.js
+++ themes/custom/cityplanning/js/api/biweeklyCaseReportService.js
@@ -1,5 +1,5 @@
 app.service('biweeklyCaseReportService', function($http) {
 	this.getBiweeklyCaseFiling = function (id) {
-		return $http.get("http://10.68.8.109:2016/ladcp/api/v1/bi-weekly-case-filing/"+id);
+		return $http.get("http://ladcp.westus.cloudapp.azure.com/api/v1/bi-weekly-case-filing/"+id);
 	}
 });
