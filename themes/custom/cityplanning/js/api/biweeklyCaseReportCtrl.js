a8a6df6ed1a58c5c0859e2ba72a38e76b0bbf01c
Humbal Shahi <sh.humble@gmail.com>
2017-10-06 16:06:01 -0700

    console the o/p

--- themes/custom/cityplanning/js/api/biweeklyCaseReportCtrl.js
+++ themes/custom/cityplanning/js/api/biweeklyCaseReportCtrl.js
@@ -1,61 +1,62 @@
 app.controller('biweeklyCaseReportCtrl', function($scope, biweeklyCaseReportService, $filter, $timeout, $http, $location) {
 	var id = $location.absUrl().split('/')[6];		// get cpc from url in the form of query parameter.
 	id = (id) ? id : 163;
+	console.log($location.absUrl().split('/'));
 
-// generate years list
+	// generate years list
 	var d = new Date();
-  var current_year = d.getFullYear();
-  var start_date = current_year;
-  var end_date = 2000;
-  var date_options = [];
-  while(start_date >= end_date) {
-    date_options.push(end_date);
-    end_date++;
-  }
+	var current_year = d.getFullYear();
+	var start_date = current_year;
+	var end_date = 2000;
+	var date_options = [];
+	while(start_date >= end_date) {
+		date_options.push(end_date);
+	    end_date++;
+	}
 
- date_options.sort(function(a, b){return b-a});
- $scope.year = {
-     model: null,
-     availableOptions: date_options,
-     selectedOption: current_year
- };
+	date_options.sort(function(a, b){return b-a});
+	$scope.year = {
+		model: null,
+		availableOptions: date_options,
+		selectedOption: current_year
+	};
 
- var self = this;
+	var self = this;
 
-  $scope.$watch('year.selectedOption', function(newDate){
-    $scope.selectedDate = newDate;
-    if(newDate) {
-      biweeklyCaseReportService.getBiweeklyCaseFiling(id, newDate).then(function(response){
-
-        $timeout(function(){
-          $scope.enable = "true";
-          $scope.loadingText = "";
-        }, 2000);
-
-        $scope.currentPage = 0;
-        $scope.pageSize = 10;
-        self.data = [];
-
-        $scope.getData = function () {
-          return $filter('filter')($scope.data, $scope.selectedDate)
-        }
-
-        $scope.numberOfPages=function(){
-          return Math.ceil($scope.getData().length/$scope.pageSize);
-        }
-
-        $scope.data = response.data;
-
-        if (id == 163) {
-          $scope.pageTitle = 'Bi-Weekly Case Filing by Certified Neighborhood Council';	//163
-        } else if (id == 173) {
-          $scope.pageTitle = 'Bi-Weekly Case Filing Activity by City Council District';	// 173
-        } else {
-          $scope.pageTitle = 'Bi-Weekly Case Filing by Community Plan Area';				// 183
-        }
-
-      });
-   }
+	$scope.$watch('year.selectedOption', function(newDate){
+		$scope.selectedDate = newDate;
+	    if(newDate) {
+	    	biweeklyCaseReportService.getBiweeklyCaseFiling(id, newDate).then(function(response){
+	
+		        $timeout(function(){
+		        	$scope.enable = "true";
+		        	$scope.loadingText = "";
+		        }, 2000);
+		
+		        $scope.currentPage = 0;
+		        $scope.pageSize = 10;
+		        self.data = [];
+		
+		        $scope.getData = function () {
+		          return $filter('filter')($scope.data, $scope.selectedDate)
+		        }
+		
+		        $scope.numberOfPages=function(){
+		          return Math.ceil($scope.getData().length/$scope.pageSize);
+		        }
+		
+		        $scope.data = response.data;
+		
+		        if (id == 163) {
+		          $scope.pageTitle = 'Bi-Weekly Case Filing by Certified Neighborhood Council';	//163
+		        } else if (id == 173) {
+		          $scope.pageTitle = 'Bi-Weekly Case Filing Activity by City Council District';	// 173
+		        } else {
+		          $scope.pageTitle = 'Bi-Weekly Case Filing by Community Plan Area';				// 183
+		        }
+		
+	      });
+	   }
  });
 });
 
