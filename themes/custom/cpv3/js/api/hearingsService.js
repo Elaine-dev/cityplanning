dca7a61ee29afc756e58ffea92b02be4ad39a400
Humbal Shahi <sh.humble@gmail.com>
2017-12-13 12:26:35 -0800

    fixed responsive for hearing and agendas

--- themes/custom/cpv3/js/api/hearingsService.js
+++
@@ -1,5 +0,0 @@
-app.service('hearingsService', function($http) {
-	this.getHearing = function (apc, year) {
-		return $http.get("http://161.149.221.137/dcpapi/meetings/api/"+apc+"/hearings/"+year);
-	}
-});
