f259d21eed23853b7d1014fdebac0397f04f89e1
Humbal Shahi <sh.humble@gmail.com>
2018-06-11 14:50:27 -0700

    deleted older themes

--- themes/custom/cityplanning/js/api/formsCtrl.js
+++
@@ -1,5 +0,0 @@
-app.controller('formsCtrl', function ($scope, formsService, $filter) {
-	formsService.getForms().then (function (response){
-		$scope.forms = response.data.items;
-	});
-});
--- themes/custom/cityplanning/js/api/formsService.js
+++
@@ -1,5 +0,0 @@
-app.service('formsService', function($http) {
-	this.getForms = function () {
-		return $http.get('http://161.149.221.137/dcpapi/general/forms');
-	}
-});
