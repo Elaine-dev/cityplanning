b0dd07acda11e55b2ea0c2480772e95eb992e30c
elaineblakeman <elaine.blakeman@gmail.com>
2019-04-02 09:44:08 -0700

    Public Counters, look mom no CORS

--- themes/custom/cpv3/js/api/publicCounters.js
+++ themes/custom/cpv3/js/api/publicCounters.js
@@ -1,129 +1,150 @@
-/**
- * File: public-counter.js
- * Author: Blakeman
- * Created date: 03/19/2019
+'use strict';
+var app = angular.module('appPubCounter', ['ngRoute', 'ngSanitize', 'ngAnimate', 'ui.bootstrap', 'ngResource', 'angular-toArrayFilter'])
+.service('pubCounterMetroService', function($http) {
+    this.getRecord = function() {
+        return $http.jsonp('https://www.ladbsservices2.lacity.org/LADBS_Services/LADBS/WaitTimes?id=7&json=true?callback', {jsonpCallbackParam: 'callback'})
+				then(function(data){
+				 $scope.pubCounterMetroServices= response.data
+				 	 console.log(response.data)
+				 }).catch(function(response) {
+ // handle errors
+				 console.log("nope");
+				 }
+    )}
+}).service('pubCounterValleyService', function($http) {
+    this.getRecord = function() {
+        return $http.jsonp('https://www.ladbsservices2.lacity.org/LADBS_Services/LADBS/WaitTimes?id=4&json=true?callback1', {jsonpCallbackParam: 'callback'})
+				then(function(data){
+         $scope.pubCounterValleyServices = response.data
+				 	 console.log(response.data)
+				 }).catch(function(response) {
+// handle errors
+				 console.log("nope");
+				 }
+       )}
+}).service('pubCounterWestlaService', function($http) {
+    this.getRecord = function() {
+        return $http.jsonp('https://www.ladbsservices2.lacity.org/LADBS_Services/LADBS/WaitTimes?id=3&json=true?callback', {jsonpCallbackParam: 'callback'})
+				then(function(data){
+				 $scope.pubCounterWestlaServices = response.data
+				 	 console.log(response.data)
+				 }).catch(function(response) {
+ // handle errors
+				 console.log("nope");
+    }
+	)}
+
+}).config(function($interpolateProvider) {
+    $interpolateProvider.startSymbol('{[{').endSymbol('}]}');
+	/*
+ // Whitelist the JSONP endpoint to show it is trusted
  */
 
-'use strict';
-var app = angular.module('appPubCounter', ['ngRoute', 'ngSanitize', 'ngAnimate', 'ui.bootstrap'])
-.service('pubCounterMetroService', function($http){
-	this.getRecord = function() {
-		return $http.get('https://www.ladbsservices2.lacity.org/LADBS_Services/LADBS/WaitTimes?id=7&json=true/');
-
-	      }
-
-		})
-		.service('pubCounterValleyService', function($http){
-			this.getRecord = function() {
-				return $http.get('https://www.ladbsservices2.lacity.org/LADBS_Services/LADBS/WaitTimes?id=4&json=true/');
-
-			      }
-
-				})
-
-				.service('pubCounterWestlaService', function($http){
-					this.getRecord = function() {
-						return $http.get('https://www.ladbsservices2.lacity.org/LADBS_Services/LADBS/WaitTimes?id=3&json=true');
-
-								}
-
-						})
+}).config(['$sceDelegateProvider', function($sceDelegateProvider) {
+		$sceDelegateProvider.resourceUrlWhitelist([
+				'self',
+				'https://www.ladbsservices2.lacity.org/LADBS_Services/LADBS/**',
+				'127.0.0.1'
+			])
 
 
-	.config(function($interpolateProvider) {
-		$interpolateProvider.startSymbol('{[{').endSymbol('}]}');
-
-	})
-  // Custom OR filters on fields of a property and can be used anywhere
-  //https://stackoverflow.com/questions/15868248/how-to-filter-multiple-values-or-operation-in-angularjs #9
-  .filter('filterWithOr', function ($filter) {
-     var comparator = function (actual, expected) {
-       if (angular.isUndefined(actual)) {
-         // No substring matching against `undefined`
-         return false;
-       }
-       if ((actual === null) || (expected === null)) {
-         // No substring matching against `null`; only match against `null`
-         return actual === expected;
-       }
-       if ((angular.isObject(expected) && !angular.isArray(expected)) || (angular.isObject(actual) && !hasCustomToString(actual))) {
-         // Should not compare primitives against objects, unless they have custom `toString` method
-         return false;
-       }
-      // console.log('ACTUAL EXPECTED')
-      // console.log(actual)
-      // console.log(expected)
-
-       actual = angular.lowercase('' + actual);
-       if (angular.isArray(expected)) {
-         var match = false;
-         expected.forEach(function (e) {
-           // console.log('forEach')
-           // console.log(e)
-           e = angular.lowercase('' + e);
-           if (actual.indexOf(e) !== -1) {
-             match = true;
-           }
-         });
-         return match;
-       } else {
-         expected = angular.lowercase('' + expected);
-         return actual.indexOf(expected) !== -1;
-       }
-     };
-     return function (array, expression) {
-       return $filter('filter')(array, expression, comparator);
-     };
-
-   })
-   //filter for formatting time in json// https://stackoverflow.com/questions/25470475/angular-js-format-minutes-in-template #7 first example
-   .filter('time', function() {
-
-   var conversions = {
-
-     'mm': function(value) { return value * 60; },
-     'hh': function(value) { return value * 3600; }
-   };
-
-   var padding = function(value, length) {
-     var zeroes = length - ('' + (value)).length,
-         pad = '';
-     while(zeroes-- > 0) pad += '0';
-     return pad + value;
-   };
-
-   return function(value, unit, format, isPadded) {
-     var totalSeconds = conversions[unit || 'ss'](value),
-         hh = Math.floor(totalSeconds / 3600),
-         mm = Math.floor((totalSeconds % 3600) / 60),
+			/*
+		 // Custom filter to filter on OR
+     // https://stackoverflow.com/questions/15868248/how-to-filter-multiple-values-or-operation-in-angularjs #9
+		 */
 
 
-     format = format || 'hh:mm';
-     isPadded = angular.isDefined(isPadded)? isPadded: true;
-     hh = isPadded? padding(hh, 2): hh;
-     mm = isPadded? padding(mm, 2): mm;
+}]).filter('filterWithOr', function($filter) {
+    var comparator = function(actual, expected) {
+        if (angular.isUndefined(actual)) {
+            return false;
+        }
+        if ((actual === null) || (expected === null)) {
+            return actual === expected;
+        }
+        if ((angular.isObject(expected) && !angular.isArray(expected)) || (angular.isObject(actual) && !hasCustomToString(actual))) {
+            return false;
+        }
+        actual = angular.lowercase('' + actual);
+        if (angular.isArray(expected)) {
+            var match = false;
+            expected.forEach(function(e) {
+                e = angular.lowercase('' + e);
+                if (actual.indexOf(e) !== -1) {
+                    match = true;
+                }
+            })
+            return match;
+        } else {
+            expected = angular.lowercase('' + expected);
+            return actual.indexOf(expected) !== -1;
+        }
+    }
+    return function(array, expression) {
+        return $filter('filter')(array, expression, comparator);
+    }
 
-     return format.replace(/hh/, hh).replace(/mm/, mm);
-   };
- });
+		/*
+	 // Custom filter to format time
+   // https://stackoverflow.com/questions/25470475/angular-js-format-minutes-in-template #7
+	 */
 
- app.controller('pubCounterMetroCtrl', ['$scope', 'pubCounterMetroService', function($scope, pubCounterMetroService){
- 	pubCounterMetroService.getRecord().then(function(response){
- 		$scope.data = response.data;
-		//console.log($scope);
-	});
-}]);
+}).filter('time', function() {
+    var conversions = {
+        'mm': function(value) {
+            return value * 60;
+        },
+        'hh': function(value) {
+            return value * 3600;
+        }
+    }
+    var padding = function(value, length) {
+        var zeroes = length - ('' + (value)).length,
+            pad = '';
+        while (zeroes-- > 0) pad += '0';
+        return pad + value;
+    }
+    return function(value, unit, format, isPadded) {
+        var totalSeconds = conversions[unit || 'ss'](value),
+            hh = Math.floor(totalSeconds / 3600),
+            mm = Math.floor((totalSeconds % 3600) / 60),
+            format = format || 'hh:mm';
+        isPadded = angular.isDefined(isPadded) ? isPadded : true;
+        hh = isPadded ? padding(hh, 2) : hh;
+        mm = isPadded ? padding(mm, 2) : mm;
+        return format.replace(/hh/, hh).replace(/mm/, mm);
+    }
+});
 
-app.controller('pubCounterValleyCtrl', ['$scope', 'pubCounterValleyService', function($scope, pubCounterValleyService){
- pubCounterValleyService.getRecord().then(function(response){
-	 $scope.data = response.data;
-	 //console.log($scope);
- });
-}]);
+app.controller('pubCounterMetroCtrl', ['$scope', 'pubCounterMetroService', function($scope,pubCounterMetroService) {
+			pubCounterMetroService.getRecord()
+	 .then(function(response) {
+		 $scope.pubCounterMetroServices = response.data
+	 	 console.log($scope.pubCounterMetroService)
+	 	 }).catch(function(data) {
+      // handle errors
+		 console.log('fail') }
+)}
+])
 
-app.controller('pubCounterWestlaCtrl', ['$scope', 'pubCounterWestlaService', function($scope, pubCounterWestlaService){
- pubCounterWestlaService.getRecord().then(function(response){
-	 $scope.data = response.data;
-	 //console.log($scope);
- });
-}]);
+app.controller('pubCounterValleyCtrl', ['$scope', 'pubCounterValleyService', function($scope,pubCounterValleyService) {
+	pubCounterValleyService.getRecord()
+.then(function(response) {
+	$scope.pubCounterValleyServices = response.data
+ console.log($scope.pubCounterValleyService)
+}).catch(function(data) {
+ // handle errors
+console.log('fail') }
+)}
+])
+
+app.controller('pubCounterWestlaCtrl', ['$scope', 'pubCounterWestlaService', function($scope,pubCounterWestlaService) {
+	pubCounterWestlaService.getRecord()
+.then(function(response) {
+	$scope.pubCounterWestlaServices = response.data
+console.log($scope.pubCounterWestlaService)
+}).catch(function(response) {
+ // handle errors
+console.log('fail') }
+)}
+]);
