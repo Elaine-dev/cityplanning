c2d1cdb22a08c7d7841566b2f8f00aa258cdfe08
Humbal Shahi <sh.humble@gmail.com>
2019-04-16 10:47:13 -0700

    deleted unnecessary files and directories

--- mockup/js/api/agendaService.js
+++
@@ -1,5 +0,0 @@
-app.service('agendaService', function($http) {
-	this.getAgenda = function(apc, year) {
-		return $http.get("https://planning.lacity.org/dcpapi/meetings/api/"+apc+"/agendas/"+year);
-	}
-});
