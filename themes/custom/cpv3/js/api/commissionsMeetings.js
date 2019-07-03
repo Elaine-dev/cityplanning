ef001ea8d70f50fd42b55de5631fd057a49514a0
Humbal Shahi <sh.humble@gmail.com>
2019-07-03 16:32:55 -0700

    Combined Commissions, Boards and Hearings in a single page using Tabs

---
+++ themes/custom/cpv3/js/api/commissionsMeetings.js
@@ -0,0 +1,181 @@
+var appCommMeetings = angular.module('appCommMeetings', ['ngSanitize', 'ngRoute', 'ngAnimate', 'ui.bootstrap'])
+	.config(function($interpolateProvider) {
+		$interpolateProvider.startSymbol('{[{').endSymbol('}]}');
+	})
+
+	.service('hearingsService', function($http) {
+		this.getHearing = function (apc, year) {
+			return $http.get("https://planning.lacity.org/dcpapi/meetings/api/"+apc+"/hearings/"+year);
+		}
+	})
+	
+	.service('boardService', function($http) {
+		this.getBoards = function(apc, year) {
+			return $http.get("https://planning.lacity.org/dcpapi/meetings/api/"+apc+"/boards/"+year);
+		}
+	})
+	
+	.service('commissionService', function($http) {
+		this.getCommissions = function(apc, year) {
+			return $http.get("https://planning.lacity.org/dcpapi/meetings/api/"+apc+"/commissions/"+year);
+		}
+	})
+	
+	// Commission Controller
+	.controller('commissionCtrl', ['$scope', '$sce', 'commissionService', '$filter', '$timeout', function($scope, $sce, commissionService, $filter, $timeout) {
+		$scope.apcAreas = {
+		   availableOptions: [ 
+		        {id: 'cpc', name: 'City Planning Commisson'},
+				{id: 'culturalheritagecommission', name: 'Cultural Heritage Commission'},
+				{id: 'northvalley', name: 'North Valley' },
+				{id: 'southvalley', name: 'South Valley'},
+				{id: 'westlosangeles', name: 'West Los Angeles'},
+				{id: 'centrallosangeles', name: 'Central Los Angeles'},
+				{id: 'eastlosangeles', name: 'East Los Angeles'},
+				{id: 'southlosangeles', name: 'South Los Angeles'},
+				{id: 'harbor', name: 'Harbor'},
+				{id: 'all', name: 'All'}
+			],
+		};
+		
+		
+		$scope.apc = 'all';
+		
+		$scope.enable = "false";
+		$scope.loadingText = "Loading ...";
+		
+		$scope.date = $filter('date')(new Date(), 'yyyy');		// get current year
+		var date = $scope.date;
+				
+		// display record according to apc
+		$scope.$watch('apc', function(newAPC) {
+			if (newAPC) { 
+				var selectedYear = $scope.date;
+								
+				commissionService.getCommissions(newAPC, selectedYear).then(function(response) {
+					$scope.agendas = response.data.Entries;
+					$scope.dates = response.data.Years;
+					$scope.planningAreaTitle = response.data.APCTitle;
+					
+					// loading icon
+					$timeout(function(){
+						$scope.enable = true;
+						$scope.loadingText = '';
+					}, 2000);					
+					
+				});
+			}
+		 });
+		
+		// display record according to date
+		$scope.$watch('date', function(newYear) {
+			if (newYear) {
+				var apc = $scope.apc;
+				commissionService.getCommissions(apc, newYear).then(function(response) {
+					$scope.agendas = response.data.Entries;
+					$scope.planningAreaTitle = response.data.APCTitle;
+				});
+			}
+		});
+	}])
+	
+	// Board Controller
+	.controller('boardCtrl', ['$scope', '$sce', 'boardService', '$filter','$timeout', function($scope, $sce, boardService, $filter, $timeout) {
+			$scope.apcAreas = {
+				availableOptions: [ 
+					{id: 'northvalley', name: 'North Valley' },
+					{id: 'southvalley', name: 'South Valley'},
+					{id: 'westlosangeles', name: 'West Los Angeles'},
+					{id: 'centrallosangeles', name: 'Central Los Angeles'},
+					{id: 'eastlosangeles', name: 'East Los Angeles'},
+					{id: 'southlosangeles', name: 'South Los Angeles'},
+					{id: 'harbor', name: 'Harbor'},
+					{id: 'all', name: 'All'}
+				],
+			};
+			
+			$scope.apc = 'all';
+		
+			$scope.enable = "false";
+			$scope.loadingText = "Loading ...";
+		
+			$scope.date = $filter('date')(new Date(), 'yyyy');		// get current year
+			var date = $scope.date;
+			
+			$scope.$watch ('apc', function(newAPC) {
+				if(newAPC) {
+					var selectedYear = $scope.date;
+					boardService.getBoards(newAPC, selectedYear).then(function (response){
+						$scope.boards = response.data.Entries;
+						$scope.dates = response.data.Years;
+						$scope.planningAreaTitle = response.data.APCTitle;
+						$timeout(function(){
+				            $scope.enable = "true";
+				           $scope.loadingText = "";
+				        }, 2000);
+					});			
+				}
+			});
+			
+		$scope.$watch('date', function(newYear){
+			if (newYear) {
+				var apc = $scope.apc;
+				boardService.getBoards(apc, newYear).then(function (response){
+					$scope.boards = response.data.Entries;
+					$scope.planningAreaTitle = response.data.APCTitle;
+				});			
+			}
+		});	
+
+	}])
+	
+	// Hearings Controller
+	.controller('hearingsCtrl', ['$scope', '$sce', 'hearingsService', '$filter','$timeout', function($scope, $sce, hearingsService, $filter, $timeout) {
+			$scope.apcAreas = {
+				availableOptions: [ 
+					{id: 'northvalley', name: 'North Valley' },
+					{id: 'southvalley', name: 'South Valley'},
+					{id: 'westlosangeles', name: 'West Los Angeles'},
+					{id: 'centrallosangeles', name: 'Central Los Angeles'},
+					{id: 'eastlosangeles', name: 'East Los Angeles'},
+					{id: 'southlosangeles', name: 'South Los Angeles'},
+					{id: 'harbor', name: 'Harbor'},
+					{id: 'all', name: 'All'}
+				],
+			};
+			
+			$scope.apc = 'all';
+		
+			$scope.enable = "false";
+			$scope.loadingText = "Loading ...";
+		
+			$scope.date = $filter('date')(new Date(), 'yyyy');		// get current year
+			var date = $scope.date;
+			$scope.assignDate = $filter('date')(new Date(), 'M/d/yyyy');
+			
+			$scope.$watch ('apc', function(newAPC) {
+				if(newAPC) {
+					var selectedYear = $scope.date;
+					hearingsService.getHearing(newAPC, selectedYear).then(function (response){
+						$scope.hearings = response.data.Entries;
+						$scope.dates = response.data.Years;
+						$scope.planningAreaTitle = response.data.APCTitle;
+						$timeout(function(){
+				            $scope.enable = "true";
+				           $scope.loadingText = "";
+				        }, 2000);
+					});			
+				}
+			});
+			
+		$scope.$watch('date', function(newYear){
+			if (newYear) {
+				var apc = $scope.apc;
+				hearingsService.getHearing(apc, newYear).then(function (response){
+					$scope.hearings = response.data.Entries;
+					$scope.planningAreaTitle = response.data.APCTitle;
+				});			
+			}
+		});	
+
+	}]);
