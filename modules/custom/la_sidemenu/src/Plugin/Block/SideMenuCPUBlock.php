c499404b15c71b50b610d7121d729104c588d841
elaineblakeman <elaine.blakeman@gmail.com>
2020-01-10 14:42:38 -0800

    Adds side menu to details page Housing

--- modules/custom/la_sidemenu/src/Plugin/Block/SideMenuCPUBlock.php
+++ modules/custom/la_sidemenu/src/Plugin/Block/SideMenuCPUBlock.php
@@ -6,11 +6,11 @@ use Drupal\Core\Block\BlockBase;
 
 /**
  * Provides 'side menu for Community Plan Update' eg. event detail pages.
- * 
+ *
  *  @Block(
  *      id = "side_menu_CPU_block",
  *      admin_label = @Translation("Community Plan Update Side Menu - Block")
- *  ) 
+ *  )
  */
 
 class SideMenuCPUBlock extends BlockBase {
@@ -20,10 +20,10 @@ class SideMenuCPUBlock extends BlockBase {
     public function build() {
         global $base_url;
         $list = array();
-        
+
         // Get URI
         $uri = $_SERVER['REQUEST_URI'];
-                       
+
         /**
          * -- Local --
          * Array (
@@ -34,48 +34,51 @@ class SideMenuCPUBlock extends BlockBase {
          [4] => pop-office-hours-appointments
          )
          */
-        
+
         $uri_array = explode('/', $uri);
-        $menu_level = $uri_array[3];                                    // tax term in the form of url   
+        $menu_level = $uri_array[3];                                    // tax term in the form of url
         $menu_level_es = ($uri_array[4]) ? $uri_array[4] : '';          // for Spanish language, es is added in url pattern
         $html = '';
-        
-        if (isset($menu_level) && $uri_array[2] == 'community-plan-update') { 
-            
+
+        //kint($menu_level);
+
+        if (isset($menu_level) && $uri_array[2] == 'community-plan-update') {
+
             $menu_base_path = '/plans-policies/community-plan-update/';
             $menu_base_path_gpu = '/plans-policies/';
             $menu_name = '';
-            
+
+
             /**
              * $menu_name: Vocabulary name in the Taxonomy. Each Community Plan Update has different menu.
              * $parent_link : URL of parent node.
-             */            
+             */
             switch ($menu_level) {
                 case 'venice-news-item':
                     $menu_name = 'venice_local_coastal_program';
                     $parent_link = $menu_base_path.'venice-local-coastal-program';
                     break;
-                    
+
                 case 'southwest-valley-news-item':
                     $menu_name = 'southwest_valley_community_plan_';
                     $parent_link = $menu_base_path.'southwest-valley-community-plans-update';
                     break;
-                    
+
                 case 'westside-news':
                     $menu_name = 'westside_community_plan_update';
                     $parent_link = $menu_base_path.'planning-westside';
                     break;
-                    
+
                 case 'westside-events':
                     $menu_name = 'westside_community_plan_update';
                     $parent_link = $menu_base_path.'planning-westside';
                     break;
-                    
+
                 case 'downtown-events':
                     $menu_name = 'downtown_community_plan_update';
                     $parent_link = $menu_base_path.'downtown-los-angeles-community-plan-update';
                     break;
-                    
+
                 case 'boyle-heights-events':
                     if ($menu_level_es == 'es'){
                         $menu_name = 'boyle_heights_cpu_menu_es';
@@ -83,70 +86,78 @@ class SideMenuCPUBlock extends BlockBase {
                     }else{
                         $menu_name = 'boyle_heights_cpu';
                         $parent_link = $menu_base_path.'boyle-heights-community-plan-update';
-                    }                    
-                    
+                    }
+
                     break;
-                    
+
                 case 'southeast-valley-news':
                     $menu_name = 'southeast_valley_menu';
                     $parent_link = $menu_base_path.'southeast-valley-community-plan-update';
-                    break; 
-                    
+                    break;
+
                 case 'harbor-events':
                     $menu_name = 'harbor_menu';
                     $parent_link = $menu_base_path.'harbor-la-community-plans-update';
                     break;
-                    
+
                 case 'harbor-events-share-phase':
                     $menu_name = 'harbor_menu';
                     $parent_link = $menu_base_path.'harbor-la-community-plans-update';
                     break;
-                    
+
                 case 'harbor-events-listen-phase':
                     $menu_name = 'harbor_menu';
                     $parent_link = $menu_base_path.'harbor-la-community-plans-update';
                     break;
-                    
+
                 case 'harbor-events-consult-phase':
                     $menu_name = 'harbor_menu';
                     $parent_link = $menu_base_path.'harbor-la-community-plans-update';
                     break;
-                    
+
                 case 'harbor-events-finalize-phase':
                     $menu_name = 'harbor_menu';
                     $parent_link = $menu_base_path.'harbor-la-community-plans-update';
                     break;
-                    
+
                 case 'harbor-events-adopt-phase':
                     $menu_name = 'harbor_menu';
                     $parent_link = $menu_base_path.'harbor-la-community-plans-update';
                     break;
-                    
+
                 case 'general-news-item':
                     $menu_name = 'gerneral_plan_update';
                     $parent_link = $menu_base_path_gpu.'general-plan-updates';
                     break;
+
+                    // path alias of node
+                    case 'housing-element-news':
+                        //taxomony term machine name
+                        $menu_name = 'housing_element_update';
+                        // url of details node one back
+                        $parent_link = $menu_base_path.'housing-element-update';
+                        break;
+
             }
-            
+
             if (!empty($menu_name)) {
                 // load menu from taxonomy
                 $menu_list = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->loadTree($menu_name);
                 $menu_format = formatTabMenuTitleForCP($menu_list);
-                
-                
+
+
                 $html = '<ul>';
                 foreach ($menu_format as $val) {
                     $html .= '<li><a href="'.$parent_link.'#'.$val['link'].'">'.$val['name'].'</a> </li>';
                     $html .= '<li class="line"></li>';
                 }
-                
+
                 $html .= '</ul>';
             }
         }
-        
+
         return [
             '#markup' => $html,
         ];
     }
 }
-
